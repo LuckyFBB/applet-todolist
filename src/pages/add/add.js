14d036bd262143a3fd409b074921c12edd7326c7
LuckyFBB <976060700@qq.com>
2019-02-17 23:54:10 +0800

    添加本地存储

--- src/pages/add/add.js
+++ src/pages/add/add.js
@@ -9,28 +9,54 @@ export default class Index extends Component {
     "enablePullDownRefresh": false
   }
 
-  componentWillMount() { }
-
-  componentDidMount() { }
-
-  componentWillUnmount() { }
-
-  componentDidShow() { }
-
-  componentDidHide() { }
+  handleSubmit = (e) => {
+    const todo = e.detail.value
+    if (!Taro.getStorageSync('todos')) {   //如果当前没有todos这个key，新建一个key值
+      let todos = []
+      Object.assign(todo, { id: 0 })   //添加唯一标识
+      console.log(todo)
+      todos.push(todo)
+      Taro.setStorage({
+        key: 'todos',
+        data: todos
+      })
+    } else {
+      Taro.getStorage({
+        key: 'todos',
+        success(res) {
+          let todos = res.data
+          Object.assign(todo, { id: todos.length })   //添加唯一标识
+          todos.push(todo)
+          Taro.setStorage({
+            key: 'todos',
+            data: todos,
+            success() {
+              Taro.navigateBack({
+                delta: 1
+              })
+              Taro.showToast({
+                title: '添加成功',
+                icon: 'success'
+              })
+            }
+          })
+        }
+      })
+    }
+  }
 
   render() {
     return (
       <View className='add'>
         <View className='add__form'>
-          <Form>
+          <Form onSubmit={this.handleSubmit}>
             <View className='form__item'>
-              <Input className='item__input' placeholder='请输入事件名称' focus={true} />
+              <Input className='item__input' placeholder='请输入事件名称' focus={true} name='name' />
             </View>
             <View className='form__item form__item--detail'>
-              <Textarea className='item__input' placeholder='事件详细描述'/>
+              <Textarea className='item__input' placeholder='事件详细描述' name='detail' />
             </View>
-            <Button className='form__button'>添加事件</Button>
+            <Button className='form__button' form-type='submit'>添加事件</Button>
           </Form>
         </View>
       </View>
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,31 +8,71 @@ export default class Index extends Component {
     enablePullDownRefresh: true
   }
 
-  componentWillMount() { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: null
+    }
+  }
+
+  componentWillMount() {
+    this.handleGetStorage()
+  }
 
   componentDidMount() { }
 
   componentWillUnmount() { }
 
-  componentDidShow() { }
+  componentDidShow() {
+    this.handleGetStorage()
+  }
 
   componentDidHide() { }
-  
-  onPullDownRefresh(){
-      console.log('添加页面')
-      Taro.navigateTo({
-        url: '../add/add'
+
+  onPullDownRefresh() {
+    console.log('添加页面')
+    Taro.navigateTo({
+      url: '../add/add'
+    })
+    Taro.stopPullDownRefresh()
+  }
+
+  handleGetStorage = () => {
+    const _this = this
+    if (Taro.getStorageSync('todos')) {
+      Taro.getStorage({
+        key: 'todos',
+        success(res) {
+          const todos = res.data
+          _this.setState({
+            todos
+          })
+        }
       })
-      Taro.stopPullDownRefresh()
+    }
   }
 
   render() {
+    const { todos } = this.state
     return (
-      <View className='index'>
-        <View className='item__none'>
-          <Text>元气满满新的一天！</Text>
-          <Text>写下新的待办事吧 ！</Text>
-        </View>
+      <View className='todo'>
+        {
+          todos === null ? (
+            <View className='todo__none'>
+              <Text>元气满满新的一天！</Text>
+              <Text>下拉创建新的待办事吧！</Text>
+            </View>
+          ) : (
+              todos.map(item => {
+                return (
+                  <View className='todo__item' key={item.id}>
+                    <Text className='item__name'>{item.name}</Text>
+                    <Text className='item__detail'>{item.detail}</Text>
+                  </View>
+                )
+              })
+            )
+        }
       </View>
     )
   }
